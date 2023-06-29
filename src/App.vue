21d816b9ceab3f5f74b938297cdefabc7c5f3a3c
Akkala Sai Mukesh Kumar <126863891+Mukeshdiksha2018@users.noreply.github.com>
2023-06-29 12:29:46 +0530

    added model of the smartphone lable into the chart

--- src/App.vue
+++ src/App.vue
@@ -17,34 +17,34 @@ export default {
   components: {
     Chart,
   },
-  data () {
-    return {
-      barName: 'Bar Chart',
-      bar: 'bar',
-      barData: {
-        labels: data.map(item => item.Brand),
-        datasets: [{
-          label: 'Price Range (INR)',
-          data: data.map(item => item['Price Range (INR)']),
-          backgroundColor: [
-            'rgba(17, 50, 64, 0.4)',
-            'rgba(94, 72, 0, 0.4)',
-            'rgba(171, 122, 232, 0.4)',
-            'rgba(27, 25, 169, 0.4)',
-            'rgba(141, 47, 108, 0.4)'
-          ],
-          borderColor: [
-            'rgba(17, 50, 64, 1)',
-            'rgba(94, 72, 0, 1)',
-            ' rgba(171, 122, 232, 1)',
-            'rgba(27, 25, 169, 1)',
-            'rgba(141, 47, 108, 1)'
-          ],
-          borderWidth: 2
-        }]
-      },
-    }
-  },
+  data() {
+  return {
+    barName: 'Bar Chart',
+    bar: 'bar',
+    barData: {
+      labels: data.map(item => item.Model),
+      datasets: [{
+        label: 'Price Range (INR)',
+        data: data.map(item => item['Price Range (INR)']),
+        backgroundColor: [
+          'rgba(17, 50, 64, 0.4)',
+          'rgba(94, 72, 0, 0.4)',
+          'rgba(171, 122, 232, 0.4)',
+          'rgba(27, 25, 169, 0.4)',
+          'rgba(141, 47, 108, 0.4)'
+        ],
+        borderColor: [
+          'rgba(17, 50, 64, 1)',
+          'rgba(94, 72, 0, 1)',
+          'rgba(171, 122, 232, 1)',
+          'rgba(27, 25, 169, 1)',
+          'rgba(141, 47, 108, 1)'
+        ],
+        borderWidth: 2
+      }]
+    },
+  }
+},
 }
 </script>
 
