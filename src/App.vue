0486cc48ef2469b40b2da2832948e768463e1ff4
Akkala Sai Mukesh Kumar <126863891+Mukeshdiksha2018@users.noreply.github.com>
2023-06-29 12:13:36 +0530

    made changes to app such that it gets JSON data

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Smartphones under 2023 {{barName}}</h1>
+    <Chart :type="bar" :data="barData" />
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Chart from './components/chart.vue';
+import data from './assets/smartphones_2023.json';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Chart,
+  },
+  data () {
+    return {
+      barName: 'Bar Chart',
+      bar: 'bar',
+      barData: {
+        labels: data.map(item => item.Brand),
+        datasets: [{
+          label: 'Price Range (INR)',
+          data: data.map(item => item['Price Range (INR)']),
+          backgroundColor: [
+            'rgba(17, 50, 64, 0.4)',
+            'rgba(94, 72, 0, 0.4)',
+            'rgba(171, 122, 232, 0.4)',
+            'rgba(27, 25, 169, 0.4)',
+            'rgba(141, 47, 108, 0.4)'
+          ],
+          borderColor: [
+            'rgba(17, 50, 64, 1)',
+            'rgba(94, 72, 0, 1)',
+            ' rgba(171, 122, 232, 1)',
+            'rgba(27, 25, 169, 1)',
+            'rgba(141, 47, 108, 1)'
+          ],
+          borderWidth: 2
+        }]
+      },
+    }
+  },
 }
 </script>
 
